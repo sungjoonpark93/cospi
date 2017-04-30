20e3d5783bb4b659cb5d5d9132757954fb344e07
SungJoon Park <SungJoon Park>
2017-04-30 17:41:31 +0900

    initial

---
+++ main.py
@@ -0,0 +1,13 @@
+
+#read gene expression data
+# df = preprocess.read_gex
+#read context label data
+# context_label_list = preprocess.read_context_label
+#read pathway data (KEGG, NCI-PID, reactome), dict
+# pathway = dict{'readtome : ['a','b','c']}
+
+
+#significant_genes = (Sereies : index : genes , values: pvalue)
+#enriched_pathway ={Series : index : pathway, values: p value}
+#COSPI =  {dict: {'pathway id:df(pathway interaction ranking)}}
+
